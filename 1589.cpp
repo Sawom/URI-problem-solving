959b22e29ab80625375709be1e249301299d8b7e
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:57:19 +0600

    Add files via upload

---
+++ 1589.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int i,T,R1,R2,result;
+  scanf("%d",&T);
+  for(i=0;i<T;i++){
+      scanf("%d%d",&R1,&R2);
+      result=R1+R2;
+      printf("%d\n",result);
+  }
+  return 0;
+}
