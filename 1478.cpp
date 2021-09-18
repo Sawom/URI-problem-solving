f5d0c2732f7f0b7de94114fa1201371bcdde2f91
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:56:15 +0600

    Add files via upload

---
+++ 1478.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int row,col,k,l,m,n;
+    for(;;){
+        scanf("%d",&n);
+        if(n==0){
+                break;
+        }
+        int ary[n][n];
+        for(row=0;row<n;row++){
+            m=row+1;
+            k=2;
+            for(col=0;col<=row;col++,m--){
+                ary[row][col]=m;
+            }
+            for(col=row+1;col<n;col++,k++){
+                ary[row][col]=k;
+            }
+        }
+        for(row=0;row<n;row++){
+            for(col=0;col<n;col++){
+                if(col==0){
+                    printf("%3d",ary[row][col]);
+                }
+                else{
+                    printf(" %3d",ary[row][col]);
+                }
+            }
+            printf("\n");
+        }printf("\n");
+    }
+    return 0;
+}
+
