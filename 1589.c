0eb40ea935d562f2de5f846274a5f7f3f5ec2a17
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:56:50 +0600

    Add files via upload

---
+++ 1589.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+  int i,T,R1,R2,result;
+  scanf("%d",&T);
+  for(i=0;i<T;i++){
+      scanf("%d%d",&R1,&R2);
+      result=R1+R2;
+      printf("%d\n",result);
+  }
+  return 0;
+}
