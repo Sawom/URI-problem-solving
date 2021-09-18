e6cd1f8b0f57547dd50e53abcf2ce8c68187c2d6
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:58:31 +0600

    Add files via upload

---
+++ 1847.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;
+    while(scanf("%d",&a)!=EOF){
+            scanf("%d%d",&b,&c);
+            if(a>b&&b<=c){
+                    printf(":)\n");
+        }else if(a<b&&b>=c){
+            printf(":(\n");
+        }else if(a<b&&b<c&&(c-b)<(b-a)){
+            printf(":(\n");
+        }else if(a<b&&b<c&&(c-b)>=(b-a)){
+            printf(":)\n");
+        }else if(a>b&&b>c&&(a-b)>(b-c)){
+            printf(":)\n");
+        }else if(a>b&&b>c&&(a-b)<=(b-c)){
+            printf(":(\n");
+        }else if(a==b){
+            if(b<c){
+                printf(":)\n");
+            }else{
+                printf(":(\n");
+            }
+        }
+    }
+}
