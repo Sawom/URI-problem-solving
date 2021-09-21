d71a38416912e894da8e312b28d1f3f3e77ad528
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-21 16:46:36 +0600

    Add files via upload

---
+++ 1834.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define size 35
+int main(){
+    int i,n;
+    char str[size]= "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    while(scanf("%d",&n)!=EOF){
+        for(i=0;i<n;i++){
+            printf("%c",str[i]);
+        }
+        printf("\n");
+    }
+return 0;
+}
